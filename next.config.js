f6b0ed09d071f4dff3db2ffec5c2ed295d04f135
satnamsingh611 <satnamsingh85611@gmail.com>
2023-11-07 17:05:30 +0530

    changes

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    env: {
+        REACT_APP_MAGIC_LINK_PUBLISH_KEY:"pk_live_0BB8F9B6052C79A4"
+      },
+}
 
 module.exports = nextConfig
